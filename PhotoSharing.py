2d83117a23b7d1b66c75d82b70359be23e941b11
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2018-11-01 00:22:43 +0900

    Add example

--- PhotoSharing.py
+++ PhotoSharing.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
+ACCESS_TOKEN = ''
+CHANNEL_SECRET = ''
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'This is photo sharing api'
+
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 
 if __name__ == '__main__':
