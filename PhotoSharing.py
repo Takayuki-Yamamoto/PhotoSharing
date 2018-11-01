c9e8d308a91d459ce3ef7f6f5df9f24ed42f7141
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2018-11-01 23:08:38 +0900

    mkdir

--- PhotoSharing.py
+++ PhotoSharing.py
@@ -67,13 +67,7 @@ def handle_message(event):
 
 
 if __name__ == '__main__':
-    try:
-        os.makedirs(static_tmp_path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
-            pass
-        else:
-            raise
+    os.makedirs(static_tmp_path)
 
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
